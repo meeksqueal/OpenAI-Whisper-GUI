d4f4899023a5412c2449ea2993c7c43c7046bf06
meeksqueal <141447225+meeksqueal@users.noreply.github.com>
2023-08-05 21:04:37 +0000

    Update main.py
    - Added the option to save the translation/transcription to a .txt, .srt, .vtt, .tsv, or .json file.
    - Added the option to directly save the translation/transcription to the input video, either as hardcoded subtitles or embedded in the .mkv format.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 import torch
 import whisper
+from whisper.utils import get_writer
 
 import customtkinter as ctk
 from customtkinter import filedialog as fd
@@ -55,7 +56,7 @@ class TopFrame(ctk.CTkFrame):
         super().__init__(master)
 
         title = ctk.CTkLabel(
-            self, text="Audio Transciber & Translater", font=("", 20, "bold")
+            self, text="Audio Transcriber & Translator", font=("", 20, "bold")
         )
         title.pack(side="left", padx=10, pady=15)
 
@@ -91,11 +92,11 @@ class LeftFrame(ctk.CTkFrame):
         model_label = ctk.CTkLabel(self, text="Model Size")
         model_label.grid(row=1, column=0, padx=10, pady=10)
 
-        model_values = ["Tiny", "Base", "Small", "Medium", "Large"]
+        model_values = ["tiny", "base", "small", "medium", "large"]
         self.model_option = ctk.CTkOptionMenu(
             self, values=model_values, corner_radius=5
         )
-        self.model_option.set("Base")
+        self.model_option.set("medium")
         self.model_option.grid(row=1, column=1, padx=10, pady=10)
 
         language_label = ctk.CTkLabel(self, text="Language")
@@ -103,105 +104,101 @@ class LeftFrame(ctk.CTkFrame):
 
         language_values = [
             "Auto Detection",
-            "english",
-            "chinese",
-            "german",
-            "spanish",
-            "russian",
-            "korean",
-            "french",
-            "japanese",
-            "portuguese",
-            "turkish",
-            "polish",
-            "catalan",
-            "dutch",
-            "arabic",
-            "swedish",
-            "italian",
-            "indonesian",
-            "hindi",
-            "finnish",
-            "vietnamese",
-            "hebrew",
-            "ukrainian",
-            "greek",
-            "malay",
-            "czech",
-            "romanian",
-            "danish",
-            "hungarian",
-            "tamil",
-            "norwegian",
-            "thai",
-            "urdu",
-            "croatian",
-            "bulgarian",
-            "lithuanian",
-            "latin",
-            "maori",
-            "malayalam",
-            "welsh",
-            "slovak",
-            "telugu",
-            "persian",
-            "latvian",
-            "bengali",
-            "serbian",
-            "azerbaijani",
-            "slovenian",
-            "kannada",
-            "estonian",
-            "macedonian",
-            "breton",
-            "basque",
-            "icelandic",
-            "armenian",
-            "nepali",
-            "mongolian",
-            "bosnian",
-            "kazakh",
-            "albanian",
-            "swahili",
-            "galician",
-            "marathi",
-            "punjabi",
-            "sinhala",
-            "khmer",
-            "shona",
-            "yoruba",
-            "somali",
             "afrikaans",
-            "occitan",
-            "georgian",
-            "belarusian",
-            "tajik",
-            "sindhi",
-            "gujarati",
+            "albanian",
             "amharic",
-            "yiddish",
-            "lao",
-            "uzbek",
-            "faroese",
-            "haitian creole",
-            "pashto",
-            "turkmen",
-            "nynorsk",
-            "maltese",
-            "sanskrit",
-            "luxembourgish",
-            "myanmar",
-            "tibetan",
-            "tagalog",
-            "malagasy",
+            "arabic",
+            "armenian",
             "assamese",
-            "tatar",
-            "hawaiian",
-            "lingala",
+            "azerbaijani",
+            "basque",
+            "belarusian",
+            "bengali",
+            "bosnian",
+            "breton",
+            "bulgarian",
+            "catalan",
+            "croatian",
+            "czech",
+            "danish",
+            "dutch",
+            "english",
+            "estonian",
+            "faroese",
+            "finnish",
+            "french",
+            "galician",
+            "georgian",
+            "german",
+            "greek",
+            "gujarati",
+            "haitian creole",
             "hausa",
-            "bashkir",
+            "hebrew",
+            "hindi",
+            "hungarian",
+            "icelandic",
+            "indonesian",
+            "italian",
+            "japanese",
             "javanese",
-            "sundanese",
+            "kannada",
+            "kazakh",
+            "khmer",
+            "korean",
+            "kurdish",
+            "kyrgyz",
+            "lao",
+            "latin",
+            "latvian",
+            "lingala",
+            "lithuanian",
+            "luxembourgish",
+            "macedonian",
+            "malagasy",
+            "malay",
+            "malayalam",
+            "maltese",
+            "maori",
+            "marathi",
+            "mongolian",
+            "myanmar",
+            "nepali",
+            "norwegian",
+            "nynorsk",
+            "occitan",
+            "pashto",
+            "persian",
+            "polish",
+            "portuguese",
+            "punjabi",
+            "romanian",
+            "russian",
+            "sanskrit",
+            "sindhi",
+            "sinhala",
+            "slovak",
+            "slovenian",
+            "somali",
+            "spanish",
+            "swahili",
+            "swedish",
+            "tagalog",
+            "tajik",
+            "tamil",
+            "tatar",
+            "telugu",
+            "thai",
+            "tibetan",
+            "turkish",
+            "turkmen",
+            "ukrainian",
+            "uzbek",
+            "vietnamese",
+            "welsh",
+            "xhosa",
+            "yoruba",
+            "zulu",
         ]
         self.language_option = ctk.CTkOptionMenu(
             self, values=language_values, corner_radius=5
@@ -214,29 +211,29 @@ class LeftFrame(ctk.CTkFrame):
 
         task_values = ["Transcribe", "Translate"]
         self.task_option = ctk.CTkOptionMenu(self, values=task_values, corner_radius=5)
-        self.task_option.set("Transcribe")
+        self.task_option.set("Translate")
         self.task_option.grid(row=3, column=1, padx=10, pady=10)
 
         self.upload_button = ctk.CTkButton(
             self,
-            text="Upload Audio",
+            text="Upload Audio/Video",
             command=self.select_file,
             border_spacing=5,
             corner_radius=5,
         )
         self.upload_button.grid(
-            row=4, column=0, padx=10, pady=10, rowspan=5, columnspan=2
+            row=5, column=0, padx=10, pady=10, rowspan=5, columnspan=2
         )
 
     def select_file(self):
         file_path = fd.askopenfilename(
             parent=self,
-            title="Select Audio File",
+            title="Select Audio/Video File",
             filetypes=(
-                ("Audio files", "*.mp3 *.wav *.flac"),
-                ("MP3 files", "*.mp3"),
-                ("WAV files", "*.wav"),
-                ("FLAC files", "*.flac"),
+                (('All files', '*.*'),
+                 ('Videofile', '*.mp4 *.avi *.mkv *.mov *.wmv *.webm *.flv'),
+                 ('Audiofile', '*.mp3 *.wav *.flac *.aac *.ogg *.wma *.m4a')
+                 )
             ),
         )
         if file_path:
@@ -252,7 +249,7 @@ class LeftFrame(ctk.CTkFrame):
                 master=self.master, text="Warning: No file is selected."
             )
             notification.show_message()
-            self.after(5000, notification.hide_message)
+            self.after(50000, notification.hide_message)
             self.file_path = None
 
     def return_data(self):
@@ -287,6 +284,8 @@ class RightFrame(ctk.CTkFrame):
 
         self.thread_pool = ThreadPoolExecutor(max_workers=5)
 
+        self.result = None
+
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
@@ -297,15 +296,20 @@ class RightFrame(ctk.CTkFrame):
             row=0, column=0, columnspan=5, padx=10, pady=10, sticky="nsew"
         )
 
+        self.subtitle_button = ctk.CTkButton(
+            self,text="Add Subtitle to Video", command=self.start_subtask, corner_radius=5
+        )
+        self.subtitle_button.grid(row=1, column=3, padx=10, pady=10)
+
         self.start_button = ctk.CTkButton(
             self, text="Start", command=self.start_task, corner_radius=5
         )
-        self.start_button.grid(row=1, column=2, padx=10, pady=10)
+        self.start_button.grid(row=1, column=1, padx=10, pady=10)
 
         self.save_button = ctk.CTkButton(
             self, text="Save", command=self.save_text, corner_radius=5
         )
-        self.save_button.grid(row=1, column=3, padx=10, pady=10)
+        self.save_button.grid(row=1, column=2, padx=10, pady=10)
 
         self.clear_button = ctk.CTkButton(
             self,
@@ -319,7 +323,42 @@ class RightFrame(ctk.CTkFrame):
         )
         self.clear_button.grid(row=1, column=4, padx=10, pady=10)
 
+    #add subtitle to video
+    def start_subtask(self):
+        if self.result is not None:
+            ogfile_name = os.path.basename(self.file_path)
+            sep = "."
+            ogfile_name = ogfile_name.split(sep,1)[0]
+            file_path = fd.asksaveasfilename(
+                parent=self,
+                initialfile= ogfile_name+"-sub",
+                defaultextension=".srt",
+                title="Export subtitle",
+                filetypes=[("MPEG-4", "*.mp4"),("MKV","*.mkv"), ("All", "*.*")],)
+            
+            file_name = os.path.basename(file_path)
+            dir_name = os.path.dirname(file_path)
+            file_extension = os.path.splitext(file_path)
+
+            tempfile_name = "tempsrt.srt"
+            writer = get_writer("srt", ".")
+            writer(self.result, tempfile_name)
+            #add the subtitle
+            if file_extension[1] == ".mkv":
+                os.system("ffmpeg -i {} -i {} -map 0 -map 1 -c copy -disposition:s:0 default -metadata:s:s:0 language={} {} -y".format('"'+self.file_path+'"',tempfile_name,self.lang, os.path.join('"'+dir_name,file_name+'"')))
+            else:
+                os.system("ffmpeg -i {} -c:v h264_nvenc -vf subtitles={} {} -y".format('"'+self.file_path+'"',tempfile_name, os.path.join('"'+dir_name,file_name+'"')))
+            
+            os.remove(os.path.join(".", tempfile_name))
+        else:
+            notification = Notification(
+                master=self.master, text="Warning: No file is transcribed."
+            )
+            notification.show_message()
+            self.after(50000, notification.hide_message)
+
     def start_task(self):
+        self.clear_output()
         if self.left_frame_ref.return_data():
             file_path, model, language, task = self.left_frame_ref.return_data()
 
@@ -332,64 +371,114 @@ class RightFrame(ctk.CTkFrame):
             master=self.master, text="Task has started. Please wait!"
         )
         notification.show_message()
+        self.after(5000, notification.destroy)
+
         self.start_button.configure(state="disabled")
+        self.file_path = file_path
+
         load_model = whisper.load_model(model)
 
         if language == "auto detection":
+            # load audio and pad/trim it to fit 30 seconds
             audio = whisper.load_audio(file_path)
             audio = whisper.pad_or_trim(audio)
 
+            # make log-Mel spectrogram and move to the same device as the model
             mel = whisper.log_mel_spectrogram(audio).to(load_model.device)
 
-            options = whisper.DecodingOptions()
-            result = whisper.decode(load_model, mel, options)
-            text = result.text.strip().capitalize()
+            # detect the spoken language
+            _, probs = load_model.detect_language(mel)
+            lang = (f"Detected language: {max(probs, key=probs.get)}")
+            self.lang = max(probs, key=probs.get)
+
+            #Notification about the detected language
+            notification = Notification(master=self.master, text=lang)
+            notification.show_message()
+
+            #transcribe the audio
+            result = load_model.transcribe(file_path, beam_size=5, best_of=2, temperature=(0.0, 0.2, 0.4, 0.6, 0.8, 1.0), verbose=False, task=task)
+            
+            #textbox output
+            for segment in result["segments"]:
+                segment = "[%s --> %s]%s" % (round(segment["start"],2),round(segment["end"],2), segment["text"])
+                self.textbox.configure(state="normal")
+                self.textbox.insert("end", segment+"\n")
+                self.textbox.configure(state="disabled")
 
         else:
-            audio = whisper.load_audio(file_path)
-            audio = whisper.pad_or_trim(audio)
-            result = load_model.transcribe(
-                audio,
-                language=language,
-                task=task,
-                fp16=torch.cuda.is_available(),
-            )
+            #transcribe the audio
+            result = load_model.transcribe(file_path, beam_size=5, best_of=2, temperature=(0.0, 0.2, 0.4, 0.6, 0.8, 1.0), language=language, verbose=False, task=task)
+            #textbox output
+            for segment in result["segments"]:
+                segment = "[%s --> %s]%s" % (round(segment["start"],2),round(segment["end"],2), segment["text"])
+                self.textbox.configure(state="normal")
+                self.textbox.insert("end", segment+"\n")
+                self.textbox.configure(state="disabled")
 
-            text = result["text"].strip().capitalize()
-
-        self.after(0, self.update_textbox, text)
+        self.result = result      
         self.start_button.configure(state="normal")
-        notification.hide_message()
+        notification.destroy()
         notification = Notification(master=self.master, text="Task complete!")
         notification.show_message()
-        self.after(5000, notification.hide_message)
 
-    def update_textbox(self, text):
-        if text is not None and text.strip():
-            self.textbox.configure(state="normal")
-            self.textbox.delete("1.0", "end")
-            self.textbox.insert("end", text)
-            self.textbox.configure(state="disabled")
 
     def save_text(self):
-        text = self.textbox.get("0.0", "end")
+        #get original filename
+        ogfile_name = os.path.basename(self.file_path)
+        sep = "."
+        ogfile_name = ogfile_name.split(sep,1)[0]
+        #ask for save as filename
         file_path = fd.asksaveasfilename(
             parent=self,
-            defaultextension=".txt",
-            title="Export text",
-            filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
-        )
-        if file_path:
-            with open(file_path, "w", encoding="utf8") as file:
-                file.write(text)
-            notification = Notification(master=self.master, text="Saving Successfully!")
-            notification.show_message()
-            self.after(5000, notification.hide_message)
+            initialfile= ogfile_name,
+            defaultextension=".srt",
+            title="Export subtitle",
+            filetypes=[("SubRip Subtitle file", "*.srt"), ("text file", "*.txt"),("Web Video Text Tracks", "*.vtt"),
+                       ("Tab-separated values", "*.tsv"),("JSON", "*.json"), ("Save all extensions", "*.all")],)
+        #get file name and file path
+        file_name = os.path.basename(file_path)
+        file_extension = os.path.splitext(file_path)
+        dir_name = os.path.dirname(file_path)
+        #output subtitle
+        if file_path and (file_extension[1]==".srt"):
+            #save as SRT file
+            writer = get_writer("srt", dir_name)
+            writer(self.result, file_name)
+            self.save_notification()
+        elif file_path and (file_extension[1]==".txt"):
+            #save as TXT file
+            txt_writer = get_writer("txt", dir_name)
+            txt_writer(self.result, file_name)
+            self.save_notification()
+        elif file_path and (file_extension[1]==".vtt"):
+            # Save as an VTT file
+            vtt_writer = get_writer("vtt", dir_name)
+            vtt_writer(self.result, file_name)
+            self.save_notification()
+        elif file_path and (file_extension[1]==".tsv"):
+            # Save as a TSV file
+            tsv_writer = get_writer("tsv", dir_name)
+            tsv_writer(self.result, file_name)
+            self.save_notification()
+        elif file_path and (file_extension[1]==".json"):
+            # Save as a JSON file
+            json_writer = get_writer("json", dir_name)
+            json_writer(self.result, file_name)
+            self.save_notification()
+        elif file_path and (file_extension[1]==".all"):
+            all_writer = get_writer("all", dir_name)
+            all_writer(self.result, file_name)
+            self.save_notification()
 
     def clear_output(self):
         self.textbox.configure(state="normal")
         self.textbox.delete("0.0", "end")
         self.textbox.configure(state="disabled")
+
+    def save_notification(self):
+        notification = Notification(master=self.master, text="Saving Successfully!")
+        notification.show_message()
+        self.after(5000, notification.hide_message)
 
 
 class BottomFrame(ctk.CTkFrame):
@@ -398,7 +487,7 @@ class BottomFrame(ctk.CTkFrame):
 
         self.grid_columnconfigure(1, weight=1)
 
-        version_label = ctk.CTkLabel(self, text="Version 1.0", font=("", 12, "italic"))
+        version_label = ctk.CTkLabel(self, text="Version 1.1", font=("", 12, "italic"))
         version_label.grid(row=0, column=0, padx=10, pady=10)
 
         github_button = ctk.CTkButton(
@@ -414,7 +503,7 @@ class BottomFrame(ctk.CTkFrame):
         github_button.grid(row=0, column=2, padx=10, pady=10)
 
     def open_github(self):
-        webbrowser.open("https://github.com/iamironman0")
+        webbrowser.open("https://github.com/meeksqueal")
 
 
 class WhisperGui(ctk.CTk):
